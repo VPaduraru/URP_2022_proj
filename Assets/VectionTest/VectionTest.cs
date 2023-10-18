d7b8833c8fb5afcb52371a7412e6e1edbf026777
VPaduraru <vpaduraru@unibz.it>
2023-10-18 20:24:16 +0200

    Push b4 disc test

---
+++ Assets/VectionTest/VectionTest.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectionTest : MonoBehaviour
+{
+    /*
+     * Write a program that prints the numbers from 1 to 100. But for multiples of three print “Vec” instead of the number and for the multiples of five print “tion”. 
+    For numbers which are multiples of both three and five print “Vection“.
+    */
+    [SerializeField]
+    private float _angle = 1f;
+    private bool _rotating = false;
+    [SerializeField]
+    private Material _a;
+    [SerializeField]
+    private Material _b;
+
+    [SerializeField]
+    private Renderer _mRenderer;
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+
+}
